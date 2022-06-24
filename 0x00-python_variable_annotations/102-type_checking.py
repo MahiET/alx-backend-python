6fcc1a2e558e5564753033b2758ecc2625b99a53
MahiET <meazalemma2020@gmail.com>
2022-06-24 08:47:26 +0000

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-""" More involved type annotations """
-from typing import Any, Union, Mapping, TypeVar
-T = TypeVar('T')
+'''Task 12's module.
+'''
+from typing import List, Tuple
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
-    """ safely get value """
-    if key in dct:
-        return dct[key]
-    else:
-        return default
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''Creates multiple copies of items in a tuple.
+    '''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
         
