24fc0b3b5b97e60c8f5d6c30f9314df4ae4561d1
MahiET <meazalemma2020@gmail.com>
2022-06-24 08:22:00 +0000

    String

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-'''Task 3's module.'''
+""" Returns the string representation of the float """
+
 
 def to_str(n: float) -> str:
-    '''Converts a floating-point number to a string.'''
-    return str(n)
+    """ takes a float n and returns the string of the float """
+    return (str(n))
